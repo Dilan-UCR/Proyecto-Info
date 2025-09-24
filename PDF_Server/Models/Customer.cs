8767981c0430e67ac79e0a6b025fdabc54476d84
Dilan Cristian Flores Martine <dilan.flores@ucr.ac.cr>
2025-09-24 10:38:13 -0600

    Integración de kafka y generación de pdf automaticamente

--- PDF_Server/Models/Customer.cs
+++
@@ -1,9 +0,0 @@
-public class Customer
-{
-    public int CustomerID { get; set; }
-    public int? PersonID { get; set; }      // FK a Person.Person
-    public int? StoreID { get; set; }
-    public int? TerritoryID { get; set; }
-    public string AccountNumber { get; set; }
-    public DateTime ModifiedDate { get; set; }
-}
